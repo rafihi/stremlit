df808b446d8653ff44005ef301d80cbab7d3f015
rafihi <155013549+rafihi@users.noreply.github.com>
2025-01-06 11:39:46 +0200

    Update stremlit_try.py

--- stremlit_try.py
+++ stremlit_try.py
@@ -1,12 +1,33 @@
 import streamlit as st
 
-st.title( "stremlit try: sunglasses:")
+st.markdown("""
+<style>
+body, html {
+    direction: RTL;
+    unicode-bidi: bidi-override;
+    text-align: right;
+}
+p, div, input, label, h1, h2, h3, h4, h5, h6 {
+    direction: RTL;
+    unicode-bidi: bidi-override;
+    text-align: right;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.title( "רמת ההשכלה בישראל")
 
 st.write("Rafi Hirsch")
+st.write("")
+st.write("")
 
-st.title("My Flourish Visualization")
+st.header("השכלה לפי פיקוח רב שנתי")
+
+st.subheader("גילאי 25-66 ##")
 
 flourish_embed_code = """
 <div class="flourish-embed flourish-chart" data-src="visualisation/21046767"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/21046767/thumbnail" width="100%" alt="chart visualization" /></noscript></div>"""
 
 st.components.v1.html(flourish_embed_code, height=600)
+
+
