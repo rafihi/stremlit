a8ff79abb46ca9a150f5b530169b0feb7642e31c
rafihi <155013549+rafihi@users.noreply.github.com>
2025-01-06 10:59:52 +0200

    Update stremlit_try.py

--- stremlit_try.py
+++ stremlit_try.py
@@ -7,6 +7,6 @@ st.write("Rafi Hirsch")
 st.title("My Flourish Visualization")
 
 flourish_embed_code = """
-<div class="<div class="flourish-embed flourish-chart" data-src="visualisation/21046767"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/21046767/thumbnail" width="100%" alt="chart visualization" /></noscript></div>"""
+<div class="flourish-embed flourish-chart" data-src="visualisation/21046767"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/21046767/thumbnail" width="100%" alt="chart visualization" /></noscript></div>"""
 
 st.components.v1.html(flourish_embed_code, height=600)
