c85ec50d650f44e6a1b7b9fdb5f27e2bc51c7b27
rafihi <155013549+rafihi@users.noreply.github.com>
2025-01-08 11:33:51 +0200

    Update stremlit_haskala.py

--- stremlit_haskala.py
+++ stremlit_haskala.py
@@ -241,32 +241,32 @@ if page == 'מפת ישראל - השכלה לפי ישוב':
     st_folium(m, width=1400, height=1000)
 
 
-url = requests.get( 
-    "https://assets2.lottiefiles.com/packages/lf20_mDnmhAgZkb.json") 
-url_json = dict() 
-if url.status_code == 200: 
-    url_json = url.json() 
-else: 
-    print("Error in URL") 
+# url = requests.get( 
+#     "https://assets2.lottiefiles.com/packages/lf20_mDnmhAgZkb.json") 
+# url_json = dict() 
+# if url.status_code == 200: 
+#     url_json = url.json() 
+# else: 
+#     print("Error in URL") 
   
   
-st.title("Adding Lottie Animation in Streamlit WebApp") 
+# st.title("Adding Lottie Animation in Streamlit WebApp") 
   
-st_lottie(url_json, 
-          # change the direction of our animation 
-          reverse=True, 
-          # height and width of animation 
-          height=400,   
-          width=400, 
-          # speed of animation 
-          speed=1,   
-          # means the animation will run forever like a gif, and not as a still image 
-          loop=True,   
-          # quality of elements used in the animation, other values are "low" and "medium" 
-          quality='high', 
-           # THis is just to uniquely identify the animation 
-          key='Car' 
-          ) 
+# st_lottie(url_json, 
+#           # change the direction of our animation 
+#           reverse=True, 
+#           # height and width of animation 
+#           height=400,   
+#           width=400, 
+#           # speed of animation 
+#           speed=1,   
+#           # means the animation will run forever like a gif, and not as a still image 
+#           loop=True,   
+#           # quality of elements used in the animation, other values are "low" and "medium" 
+#           quality='high', 
+#            # THis is just to uniquely identify the animation 
+#           key='Car' 
+#           ) 
 
 
 
